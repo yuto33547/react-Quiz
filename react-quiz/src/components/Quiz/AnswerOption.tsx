2f9f423af1d23562a1c463bbcc6fa152797cb6c5
yuto33547 <monogatari9010@gmail.com>
2024-04-02 00:29:45 +0900

    HTMLを修正　高さの調節

--- react-quiz/src/components/Quiz/AnswerOption.tsx
+++ react-quiz/src/components/Quiz/AnswerOption.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function AnswerOption({ text, top }) {
+function AnswerOption({ text }) {
   return (
-    <div className="AnswerOption" style={{ position: "absolute", top: top }}>
-      <div className="Rectangle5 w-[300px] h-[60px] left-0 top-0 bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden">
-        <div className=" left-[13px] top-[19px] text-center text-zinc-800 text-xl font-medium font-['DM Sans']">
+    <div className="AnswerOption">
+      <div className="Rectangle5 w-[300px] h-[60px] bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4">
+        <div className=" text-center text-zinc-800 text-xl font-medium font-['DM Sans']">
           {text}
         </div>
       </div>
--- react-quiz/src/components/Quiz/Quiz.tsx
+++ react-quiz/src/components/Quiz/Quiz.tsx
@@ -13,45 +13,38 @@ function Quiz() {
     getQuestions();
   }, []);
   return (
-    <>
-      <div className="QuizAppUiDesign min-h-screen p-4 max-w-[800px] m-auto ">
-        <div className="Frame w-[90%] h-[85%] left-[5%] top-[1%] bottom-[1%] absolute bg-white rounded-[30px]">
-          <div className="flex flex-col h-screen">
-            <div className="h-50 bg-red-500 flex items-center justify-center">
-              <div className="QuestionBox w-64 h-18 bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4">
-                {/* QuestionBox */}
-                <div className="Group1 flex items-center justify-center">
-                  <div className="ml-4 text-fuchsia-700 text-xl font-bold font-['DM Sans']">
-                    18
-                  </div>
-                </div>
-                <div className="text-fuchsia-700 text-sm font-medium font-['DM Sans'] mb-2">
-                  Question 13/20
-                </div>
-                <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans']">
-                  坂本龍馬の名言はどれ？
-                </div>
-              </div>
+    <div className="QuizApp min-h-screen p-4 m-auto flex-col">
+      <div className="QuestionContainer  h-50 bg-red-500 flex items-center justify-center">
+        <div className="QuestionBox  bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4">
+          <div className="Group1 flex items-center justify-center">
+            <div className="ml-4 text-fuchsia-700 text-xl font-bold font-['DM Sans']">
+              18
             </div>
-
-            <div className="AnswerOptions flex-1 bg-blue-500 flex flex-col justify-center items-center relative">
-              <AnswerOption text="我がなすことは我のみぞ知る" top="5%" />
-              <AnswerOption text="天は人の上に人を作らず" top="30%" />
-              <AnswerOption text="人事を尽くして天命を待つ" top="55%" />
-              <AnswerOption text="明日の我に今日は勝つべし" top="80%" />
-            </div>
-
-            <div className="h-40 bg-green-500 flex justify-center items-center">
-              <div className="Button p-2.5 left-[35%] top-[73%] bg-fuchsia-700 bg-opacity-50 rounded-xl gap-2.5 inline-flex">
-                <div className="text-center text-black text-xl font-medium font-['DM Sans']">
-                  送信する
-                </div>
-              </div>
-            </div>
+          </div>
+          <div className="text-fuchsia-700 text-sm font-medium font-['DM Sans'] mb-2">
+            Question 13/20
+          </div>
+          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] p-2">
+            坂本龍馬の名言はどれ？
           </div>
         </div>
       </div>
-    </>
+
+      <div className="AnswerOptionsContainer flex-1 bg-blue-500 flex items-center justify-center flex-col">
+        <AnswerOption text="我がなすことは我のみぞ知る" />
+        <AnswerOption text="天は人の上に人を作らず" />
+        <AnswerOption text="人事を尽くして天命を待つ" />
+        <AnswerOption text="明日の我に今日は勝つべし" />
+      </div>
+
+      <div className="Submit h-40 bg-green-500 flex justify-center items-center">
+        <div className="Button p-2.5 bg-fuchsia-700 bg-opacity-50 rounded-xl justify-center items-center gap-2.5 inline-flex w-[30%] h-[10%] ">
+          <div className="text-center text-black text-xl font-medium font-['DM Sans']">
+            送信する
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
