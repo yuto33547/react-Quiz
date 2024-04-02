5e08853d66c270f662f7d4f3861f8fc76f7ef1a5
yuto33547 <monogatari9010@gmail.com>
2024-04-02 23:20:24 +0900

    h-[auto]で文字数に合わせて大きさを変えた

--- react-quiz/src/components/Quiz/AnswerOption.tsx
+++ react-quiz/src/components/Quiz/AnswerOption.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 function AnswerOption({ text }) {
   return (
-    <div className="AnswerOption">
-      <div className="Rectangle5 w-[300px] h-[60px] bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4">
-        <div className=" text-center text-zinc-800 text-xl font-medium font-['DM Sans']">
-          {text}
-        </div>
+    <div className="Rectangle5 bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[80%] h-[auto]">
+      <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
+        {text}
       </div>
     </div>
   );
