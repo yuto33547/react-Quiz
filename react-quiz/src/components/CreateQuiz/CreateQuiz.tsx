68eba987136b1558d59a1eebd2fa95ea01314280
yuto33547 <monogatari9010@gmail.com>
2024-04-05 01:22:16 +0900

    Update CreateQuiz.tsx
    Stateで問題を持ちオンオフのトグルを追加。送信押すとDBに登録できるようにしている途中

--- react-quiz/src/components/CreateQuiz/CreateQuiz.tsx
+++ react-quiz/src/components/CreateQuiz/CreateQuiz.tsx
@@ -1,48 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const CreateQuiz = () => {
+  const [question, setQuestion] = useState("");
+  const [answerOptions, setAnswerOptions] = useState([
+    { text: "", isOn: false },
+    { text: "", isOn: false },
+    { text: "", isOn: false },
+    { text: "", isOn: false },
+  ]);
+
+  const createQuestion = async () => {
+    try {
+      const docRef = doc(db, "Questions", "Q3");
+      const answerOptionsText = answerOptions.map((option) => option.text);
+      await setDoc(docRef, {
+        A: "4",
+        Q: question,
+        AnswerOptions: answerOptionsText,
+      });
+      console.log("問題Q3を追加しました");
+    } catch (error) {
+      console.error("ドキュメントの追加中にエラーが発生しました: ", error);
+    }
+  };
+
+  const handleAnswerOptionChange = (index, newText) => {
+    const updatedAnswerOptions = [...answerOptions];
+    updatedAnswerOptions[index].text = newText;
+    setAnswerOptions(updatedAnswerOptions);
+  };
+
+  const toggleButton = (index) => {
+    const updatedAnswerOptions = [...answerOptions];
+    updatedAnswerOptions[index].isOn = !updatedAnswerOptions[index].isOn;
+    setAnswerOptions(updatedAnswerOptions);
+  };
+
   return (
-    <div className="QuizApp min-h-screen p-4 m-auto flex-col">
-      <div className="QuestionContainer  flex-1 bg-red-500 flex items-center justify-center">
-        <div className="QuestionBox  bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4">
-          <div className="text-fuchsia-700 text-sm font-medium font-['DM Sans'] mb-2">
-            Question 13/20
-          </div>
-          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] p-2 h-[auto]">
-            坂本龍馬の名言はどれ？
-          </div>
+    <div className="QuizApp min-h-screen p-4 m-auto flex flex-col">
+      <div className="QuestionContainer flex-1 bg-red-500 flex items-center justify-center">
+        <div className="QuestionBox bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4 p-2">
+          <input
+            type="text"
+            placeholder="問題を入力して下さい"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            className="text-zinc-800 text-xl font-medium font-['DM Sans'] w-full p-2"
+          />
         </div>
       </div>
 
       <div className="AnswerOptionsContainer flex-1 bg-blue-500 flex items-center justify-center flex-col">
-        <div className="item bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]">
-          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
-            我がなすことは我のみぞ知るでも坂本りょうはそんなことは知らずに生活していたし、おりょうはもっと何も知らなかったに違いない。
+        {answerOptions.map((option, index) => (
+          <div
+            key={index}
+            className="item bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%]"
+          >
+            <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] p-2">
+              <input
+                type="text"
+                placeholder="回答を入力して下さい"
+                value={option.text}
+                onChange={(e) =>
+                  handleAnswerOptionChange(index, e.target.value)
+                }
+                className="w-full"
+              />
+              <button
+                onClick={() => toggleButton(index)}
+                className={`rounded-full w-8 h-8 flex items-center justify-center text-white font-bold text-lg ${
+                  option.isOn ? "bg-green-500" : "bg-red-500"
+                }`}
+              >
+                {option.isOn ? "ON" : "OFF"}
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="item bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]">
-          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
-            天は人の上に人を作らず
-          </div>
-        </div>
-        <div className="item bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]">
-          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
-            人事を尽くして天命を待つ
-          </div>
-        </div>
-        <div className="item bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]">
-          <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
-            明日の我に今日は勝つべし
-          </div>
-        </div>
+        ))}
       </div>
 
       <div className="Submit flex-1 bg-green-500 flex justify-center items-center">
-        <div className="Button p-2.5 bg-fuchsia-700 bg-opacity-50 rounded-xl justify-center items-center gap-2.5 inline-flex w-[30%] h-[10%] mb-4 mt-4">
+        <button
+          className="Button px-2.5 py-2.5 bg-fuchsia-700 bg-opacity-50 rounded-xl justify-center items-center inline-flex w-[30%] h-[10%] mb-4 mt-4 gap-2.5"
+          type="button"
+          onClick={createQuestion}
+        >
           <div className="text-center text-black text-xl font-medium font-['DM Sans']">
             送信する
           </div>
-        </div>
+        </button>
       </div>
     </div>
   );
