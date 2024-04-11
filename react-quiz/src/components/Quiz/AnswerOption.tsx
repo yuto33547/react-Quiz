f0e8fd5139a9f239796ed85cabbc5ab1c5dbda63
yuto33547 <monogatari9010@gmail.com>
2024-04-12 00:19:32 +0900

    選択した回答のインデックスをstateで保持

--- react-quiz/src/components/Quiz/AnswerOption.tsx
+++ react-quiz/src/components/Quiz/AnswerOption.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
 interface AnswerOptionProps {
+  index: number;
   text: string;
+  setChooseAnswer: (index: number) => void;
 }
 
 function AnswerOption(props: AnswerOptionProps) {
+  let { index, text, setChooseAnswer } = props;
+  const chooseAnswer = () => {
+    //選択した回答の色を変える処理
+
+    setChooseAnswer(index);
+  };
+
   return (
-    <div className="Rectangle5 bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]">
+    <div
+      className="Rectangle5 bg-white rounded-[15px] border-2 border-fuchsia-700 overflow-hidden mb-4 mt-4 w-[90%] h-[auto]"
+      onClick={chooseAnswer}
+    >
       <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] mb-4 mt-4">
-        {props.text}
+        {text}
       </div>
     </div>
   );
--- react-quiz/src/components/Quiz/Quiz.tsx
+++ react-quiz/src/components/Quiz/Quiz.tsx
@@ -20,6 +20,8 @@ interface Question {
 function Quiz() {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [chooseAnswer, setChooseAnswer] = useState<number | undefined>();
+
   useEffect(() => {
     // ここでFirestoreからデータを取得する処理を実行
     const getQuestions = async () => {
@@ -30,10 +32,16 @@ function Quiz() {
         newQuestions.push(doc.data() as Question);
       });
       setQuestions(newQuestions);
+      console.log("firestoreを読み込みました");
     };
     getQuestions();
   }, []);
   console.log(questions);
+  console.log(chooseAnswer);
+
+  const checkAnswer = () => {
+    //回答をチェック
+  };
 
   return (
     <>
@@ -45,13 +53,13 @@ function Quiz() {
               key={index}
             >
               <div className="QuestionContainer  flex-1 bg-red-500 flex items-center justify-center">
-                <div className="QuestionBox  bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4">
+                <div className="QuestionBox w-[60%] bg-white rounded-lg shadow border border-black border-opacity-10 mb-4 mt-4">
                   <div className="Group1 flex items-center justify-center">
                     <div className="ml-4 text-fuchsia-700 text-xl font-bold font-['DM Sans']">
                       18
                     </div>
                   </div>
-                  <div className="text-fuchsia-700 text-sm font-medium font-['DM Sans'] mb-2">
+                  <div className="text-fuchsia-700 text-sm font-medium font-['DM Sans'] mb-2 text-center">
                     Question {index + 1}/{questions.length}
                   </div>
                   <div className="text-center text-zinc-800 text-xl font-medium font-['DM Sans'] p-2 h-[auto]">
@@ -61,22 +69,26 @@ function Quiz() {
               </div>
 
               <div className="AnswerOptionsContainer flex-1 bg-blue-500 flex items-center justify-center flex-col">
-                {question.AnswerOptions.map((answerOptionText) => (
-                  <AnswerOption text={answerOptionText} />
+                {question.AnswerOptions.map((answerOptionText, index) => (
+                  <AnswerOption
+                    key={index}
+                    index={index}
+                    text={answerOptionText}
+                    setChooseAnswer={setChooseAnswer}
+                  />
                 ))}
-
-                {/* <AnswerOption text="我がなすことは我のみぞ知るでも坂本りょうはそんなことは知らずに生活していたし、おりょうはもっと何も知らなかったに違いない。" />
-                <AnswerOption text="天は人の上に人を作らず" />
-                <AnswerOption text="人事を尽くして天命を待つ" />
-                <AnswerOption text="明日の我に今日は勝つべし" /> */}
               </div>
 
               <div className="Submit flex-1 bg-green-500 flex justify-center items-center">
-                <div className="Button p-2.5 bg-fuchsia-700 bg-opacity-50 rounded-xl justify-center items-center gap-2.5 inline-flex w-[30%] h-[10%] mb-4 mt-4">
+                <button
+                  className="Button px-2.5 py-2.5 bg-fuchsia-700 bg-opacity-50 rounded-xl w-[30%] h-[10%] mb-4 mt-4 text-center"
+                  type="button"
+                  onClick={checkAnswer}
+                >
                   <div className="text-center text-black text-xl font-medium font-['DM Sans']">
-                    送信する
+                    回答する
                   </div>
-                </div>
+                </button>
               </div>
             </div>
           );
